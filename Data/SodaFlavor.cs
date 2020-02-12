9f26b388561eb58784a7c2ba6d34322339379314
cash_f <cffitzgi@gmail.com>
2020-02-12 15:29:29 -0600

    new file:   Data/Drink.cs 	new file:   Data/SodaFlavor.cs

---
+++ Data/SodaFlavor.cs
@@ -0,0 +1,20 @@
+/* SodaFlavor.cs
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// Soda flavor enumerator;
+    /// </summary>
+    public enum SodaFlavor
+    {
+        Cream,
+        Orange,
+        Sarsaparilla,
+        Birch,
+        Root,
+    }
+}
