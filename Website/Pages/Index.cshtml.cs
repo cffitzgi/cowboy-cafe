9ee15fd296ec1ce7298b677805b4430b5d37e237
Cashel Fitzgibbons <cffitzgi@ksu.edu>
2020-05-06 19:28:26 -0500

    Jerked Soda Flavors added to listed Jerked Soda item. Display was made responsive according to guidelines and my own display testing. Search/Filter functionality was replaced by LINQ, I didn't want to delete the methods I wrote last week though.

--- Website/Pages/Index.cshtml.cs
+++ Website/Pages/Index.cshtml.cs
@@ -26,49 +26,113 @@ namespace CowboyCafe.Website.Pages
         /// <summary>
         /// Search terms to filter for
         /// </summary>
+
+        [BindProperty(SupportsGet = true)]
         public string SearchTerms { get; set; } = "";
 
         /// <summary>
         /// Categories to filter for
         /// </summary>
+        [BindProperty(SupportsGet = true)]
         public string[] Categories { get; set; }
 
         /// <summary>
         /// Minimum amount of calories to filter for
         /// </summary>
+        [BindProperty(SupportsGet = true)]
         public int? CalMin { get; set; }
 
         /// <summary>
         /// Maximum amount of calories to filter for
         /// </summary>
+        [BindProperty(SupportsGet = true)]
         public int? CalMax { get; set; }
 
         /// <summary>
         /// Minimum price to filter for
         /// </summary>
+        [BindProperty(SupportsGet = true)]
         public double? PriceMin { get; set; }
 
         /// <summary>
         /// Maximum price to filter for
         /// </summary>
+        [BindProperty(SupportsGet = true)]
         public double? PriceMax { get; set; }
 
         /// <summary>
         /// On search.
         /// </summary>
-        public void OnGet(double? priceMin, double? priceMax, int? calMin, int? calMax)
+        public void OnGet()
         {
-            SearchTerms = Request.Query["SearchTerms"];
-            Categories = Request.Query["Categories"];
-            PriceMin = priceMin;
-            PriceMax = priceMax;
-            CalMin = calMin;
-            CalMax = calMax;
 
-            MenuList = Menu.Search(Menu.CompleteMenu, SearchTerms);
-            MenuList = Menu.FilterByCategory(MenuList, Categories);
-            MenuList = Menu.FilterByPrice(MenuList, PriceMin, PriceMax);
-            MenuList = Menu.FilterByCalories(MenuList, CalMin, CalMax);
+            MenuList = Menu.CompleteMenu;
+
+            if (Categories != null && Categories.Length > 0)
+            {
+                List<IOrderItem> categoriesList = new List<IOrderItem>();
+
+                if (Categories.Contains("Entrees")) categoriesList.AddRange(MenuList.Where(item => item is Entree));
+                if (Categories.Contains("Sides")) categoriesList.AddRange(MenuList.Where(item => item is Side));
+                if (Categories.Contains("Drinks")) categoriesList.AddRange(MenuList.Where(item => item is Drink));
+
+                MenuList = categoriesList;
+            }
+
+            if (SearchTerms != null)
+            {
+                MenuList = MenuList.Where(item => item.ToString() != null && item.ToString().Contains(SearchTerms, StringComparison.CurrentCultureIgnoreCase));
+            }
+
+            if (PriceMin != null)
+            {
+                MenuList = MenuList.Where(item => {
+                    if (item is Side side)
+                    {
+                        side.Size = Size.Large;
+                        bool largePrice = PriceMin <= side.Price;
+                        side.Size = Size.Small;
+                        return largePrice;
+                    }
+                    else if (item is Drink drink)
+                    {
+                        drink.Size = Size.Large;
+                        bool largePrice = PriceMin <= drink.Price;
+                        drink.Size = Size.Small;
+                        return largePrice;
+                    }
+                    return PriceMin <= item.Price;
+                });
+            }
+            if (PriceMax != null)
+            {
+                MenuList = MenuList.Where(item => item.Price <= PriceMax);
+            }
+
+            if (CalMin != null)
+            {
+                MenuList = MenuList.Where(item => {
+                    if (item is Side side)
+                    {
+                        side.Size = Size.Large;
+                        bool largeCal = CalMin <= side.Calories;
+                        side.Size = Size.Small;
+                        return largeCal;
+                    }
+                    else if (item is Drink drink)
+                    {
+                        drink.Size = Size.Large;
+                        bool largeCal = CalMin <= drink.Calories;
+                        drink.Size = Size.Small;
+                        return largeCal;
+                    }
+                    return CalMin <= item.Calories;
+                });
+            }
+            if (CalMax != null)
+            {
+                MenuList = MenuList.Where(item => item.Calories <= CalMax);
+            }
         }
     }
 }
